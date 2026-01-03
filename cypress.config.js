93be103183b5c736b018c4352015dec5eebaaa72
AstafevaAnastasia <privet200426@gmail.com>
2026-01-03 23:53:45 +0400

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000', // Замените на URL вашего приложения
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    projectId: 'ivhf75',
+  },
+}
